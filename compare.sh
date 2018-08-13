fdc7d24bed2c014a9752bf8a6bf51e0ee9c119ce
Samuel Albrecht <elsamuko@gmail.com>
2018-08-13 21:52:02 +0200

    chg: win: run compare.sh on win, too

--- compare.sh
+++ compare.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
+# compare fsrc with ripgrep
+
+case $(uname) in
+    Linux)
+        BOOST_DIR="/usr/include/boost/"
+        ;;
+    Darwin)
+        BOOST_DIR="/usr/include/boost/"
+        ;;
+    CYGWIN*)
+        BOOST_DIR="C:\\ProgramData\\boost"
+        ;;
+    *)
+        echo "Unknown OS"
+        ;;
+esac
+
 
 FSRC="$PWD/fsrc"
 
 echo
 echo "fsrc"
-(cd "/usr/include/boost/" && ($FSRC filesystem | tail -n 1))
+(cd "$BOOST_DIR" && ($FSRC filesystem | tail -n 1))
 echo
 echo "rg"
-(cd "/usr/include/boost/" && (rg --stats filesystem | tail -n 3 ))
+(cd "$BOOST_DIR" && (rg --stats filesystem | tail -n 3 ))
 echo
 
