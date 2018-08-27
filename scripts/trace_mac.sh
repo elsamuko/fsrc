2a777f51ffe9f2012dfa190681a0695db4063ee0
Samuel Albrecht <elsamuko@gmail.com>
2018-08-27 21:26:59 +0200

    chg: beautified trace_mac.sh

--- scripts/trace_mac.sh
+++ scripts/trace_mac.sh
@@ -4,6 +4,6 @@ TERM="test"
 SEARCH_DIR="/usr/include/netinet"
 echo "Searching in $SEARCH_DIR for $TERM"
 
-sudo dtruss -e -c ./fsrc -d "$SEARCH_DIR" --no-git "$TERM"  2> dtrace_fsrc.log
-sudo dtruss -e -c rg -u $TERM $SEARCH_DIR 2> dtrace_rg.log
+sudo dtruss -eo -c ./fsrc -d "$SEARCH_DIR" --no-git "$TERM" 2> >(grep -v "dtrace:" > dtrace_fsrc.log)
+sudo dtruss -eo -c rg -u "$TERM" "$SEARCH_DIR"              2> >(grep -v "dtrace:" > dtrace_rg.log)
 
