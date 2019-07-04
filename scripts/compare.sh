be3e0e5ad447691087d2d6676ed7e080c73a62de
Samuel Albrecht <elsamuko@gmail.com>
2019-07-05 01:46:58 +0200

    chg: compare.sh with function green

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 # compare fsrc with ripgrep
 
+function green  {
+    echo -e "\033[1;32m$1\033[0m"
+}
+
 SEARCH_DIR="libs/boost/include"
 TERM="filesystem"
-TIMEFORMAT=%R
+TIMEFORMAT="%R"
 
 echo "Searching in $SEARCH_DIR for $TERM"
 
 echo
-echo -e "\033[1;32m===FSRC===\033[0m"
+green "===FSRC==="
 echo "./fsrc -t \"$TERM\" --no-colors -d \"$SEARCH_DIR\""
 time ./fsrc -t "$TERM" --no-colors -d "$SEARCH_DIR" | tail -n 2 | grep .
 echo
 
-echo -e "\033[1;32m====RG====\033[0m"
+green "====RG===="
 echo "rg --stats -u -e \"$TERM\" \"$SEARCH_DIR\""
 time rg --stats -u -e "$TERM" "$SEARCH_DIR" | tail -n 8
 echo
 
-echo -e "\033[1;32m===FIND===\033[0m"
+green "===FIND==="
 echo "find \"$SEARCH_DIR\" -type f | wc -l"
 time find "$SEARCH_DIR" -type f | wc -l
 echo
