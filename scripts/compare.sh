f7c5743e42b2ee3ab377c5f9737ba5ef77ee8f46
Samuel Albrecht <elsamuko@gmail.com>
2019-07-05 01:38:57 +0200

    chg: always search in boost include dir with compare.sh

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,42 +1,24 @@
 #!/usr/bin/env bash
 # compare fsrc with ripgrep
 
-case $(uname) in
-    Linux)
-        SEARCH_DIR="/usr/include/"
-        ;;
-    Darwin)
-        SEARCH_DIR="/usr/include/"
-        alias grep=ggrep
-        ;;
-    CYGWIN*)
-        SEARCH_DIR='C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC'
-        ;;
-    *)
-        echo "Unknown OS"
-        ;;
-esac
+SEARCH_DIR="libs/boost/include"
+TERM="filesystem"
+TIMEFORMAT=%R
 
-TERM="test"
 echo "Searching in $SEARCH_DIR for $TERM"
 
 echo
-echo "===FSRC==="
+echo -e "\033[1;32m===FSRC===\033[0m"
 echo "./fsrc -t \"$TERM\" --no-colors -d \"$SEARCH_DIR\""
-./fsrc -t "$TERM" --no-colors -d "$SEARCH_DIR" | tail -n 2 | grep .
+time ./fsrc -t "$TERM" --no-colors -d "$SEARCH_DIR" | tail -n 2 | grep .
 echo
 
-echo "====RG===="
+echo -e "\033[1;32m====RG====\033[0m"
 echo "rg --stats -u -e \"$TERM\" \"$SEARCH_DIR\""
-rg --stats -u -e "$TERM" "$SEARCH_DIR" | tail -n 8
+time rg --stats -u -e "$TERM" "$SEARCH_DIR" | tail -n 8
 echo
 
-# echo "===GREP==="
-# echo "grep -qnr \"$TERM\" \"$SEARCH_DIR\""
-# time grep -qnr "$TERM" "$SEARCH_DIR" 2>&1 | grep real
-# echo
-
-echo "===FIND==="
+echo -e "\033[1;32m===FIND===\033[0m"
 echo "find \"$SEARCH_DIR\" -type f | wc -l"
-find "$SEARCH_DIR" -type f | wc -l
+time find "$SEARCH_DIR" -type f | wc -l
 echo
