9b6f0c7c1836c8984a1f97727bbef11edc9daefc
Samuel Albrecht <elsamuko@gmail.com>
2019-07-09 17:47:34 +0200

    chg: iterate matches instead lines in searcher

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -102,57 +102,66 @@ std::vector<Searcher::Print> Searcher::collectPrints( const sys_string& path, co
 
     // parse file for newlines until last match
     long long stop = matches.back().second - content.cbegin();
-    utils::Lines lines = utils::parseContent( content.data(), content.size(), stop );
+    const utils::Lines lines = utils::parseContent( content.data(), content.size(), stop );
+
+    size_t lineNo = 0;
+    size_t size = lines.size();
+    size_t printed = size + 1; // init with unreachable line number
 
     std::vector<Match>::const_iterator match = matches.cbegin();
-    size_t size = lines.size();
+    std::vector<Match>::const_iterator end = matches.cend();
 
-    for( size_t i = 0; i < size; ++i ) {
-        Iter from = lines[i].cbegin();
-        Iter to   = lines[i].cend();
-        bool printedLine = false;
+    for( ; match != end; ) {
 
-        // find all matches in this line
-        while( from <= match->first && match->first < to ) {
+        // find line for match
+        while( !( match->first < lines[lineNo].cend() ) ) {
+            ++lineNo;
+        }
 
-            // print line information in blue once per line
-            if( !printedLine ) {
-                printedLine = true;
-                std::string number = utils::format( "\nL%4i : ", i + 1 );
-                prints.emplace_back( utils::printFunc( cblue, number ) );
+        assert( lineNo < size );
+
+        const std::string_view& line = lines[lineNo];
+
+        // print lineNo and code until match start
+        if( printed != lineNo ) {
+            printed = lineNo;
+
+            // line in blue
+            std::string number = utils::format( "\nL%4i : ", lineNo + 1 );
+            prints.emplace_back( utils::printFunc( cblue, number ) );
+
+            // code in neutral
+            if( line.cbegin() < match->first ) {
+                prints.emplace_back( utils::printFunc( Color::Neutral, std::string( line.cbegin(), match->first ) ) );
+            }
+        }
+
+
+        // print match in red
+        prints.emplace_back( utils::printFunc( cred, std::string( match->first, match->second ) ) );
+
+        // set from to end of match
+        Iter from = match->second;
+
+        // if there are no more matches in this file, print rest of line in neutral
+        // and exit search for this file
+        if( std::next( match ) == end ) {
+            if( from < line.cend() ) {
+                prints.emplace_back( utils::printFunc( Color::Neutral, std::string( from, line.cend() ) ) );
             }
 
-            // print code in neutral
+            goto end;
+        }
+
+        ++match;
+
+        // if next match is within this line, print code in neutral until next match
+        if( match->first < line.cend() ) {
             prints.emplace_back( utils::printFunc( Color::Neutral, std::string( from, match->first ) ) );
-
-            // print match in red
-            prints.emplace_back( utils::printFunc( cred, std::string( match->first, match->second ) ) );
-
-            // set from to end of match
-            from = match->second;
-
-            // if there are no more matches in this file, print rest of line in neutral
-            // and exit search for this file
-            if( std::next( match ) == matches.cend() ) {
-                if( from < to ) {
-                    prints.emplace_back( utils::printFunc( Color::Neutral, std::string( from, to ) ) );
-                }
-
-                goto end;
-            }
-
-            ++match;
-
-            // if next match is within this line, print code in neutral until next match
-            if( from <= match->first && match->first < to ) {
-                prints.emplace_back( utils::printFunc( Color::Neutral, std::string( from, match->first ) ) );
-                from = match->first;
-            }
-            // else print code in neutral until end and break to next line
-            else {
-                prints.emplace_back( utils::printFunc( Color::Neutral, std::string( from, to ) ) );
-                break;
-            }
+        }
+        // else print code in neutral until end
+        else {
+            prints.emplace_back( utils::printFunc( Color::Neutral, std::string( from, line.cend() ) ) );
         }
     }
 
