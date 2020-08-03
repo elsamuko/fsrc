f9bb1844ab153a1fb27753e498a83fe6d384a6b6
Samuel Albrecht <elsamuko@gmail.com>
2020-08-03 17:38:20 +0200

    fix: cmake deployment

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,17 +2,19 @@
 if(APPLE)
     # on macOS, use own threadpool
     add_definitions(-DTHREADPOOL=OWN_THREADPOOL)
+    add_definitions(-DFIND_ALGO=FIND_MISCHASAN)
 endif()
 
 if(UNIX AND NOT APPLE)
     # on Linux, use boost::asio
-    add_definitions(-DTHREADPOOL=BOOST_THREADPOOL)
+    add_definitions(-DTHREADPOOL=OWN_THREADPOOL)
+    add_definitions(-DFIND_ALGO=FIND_STRSTR)
 endif()
 
 if(WIN32)
     # on Windows, use std::async
     add_definitions(-DTHREADPOOL=ASYNC_THREADPOOL)
+    add_definitions(-DFIND_ALGO=FIND_STRSTR)
 endif()
 
 add_definitions(-DDETAILED_STATS=1) # if 1, print detailed times
-add_definitions(-DWITH_SSE=1) # if 1, use mischasan's sse optimized string search
