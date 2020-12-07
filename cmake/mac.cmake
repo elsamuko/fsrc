80cd9e20bd53c8258ed6026e5cad49887341dbbd
Samuel Albrecht <elsamuko@gmail.com>
2020-12-07 22:47:35 +0100

    chg: mac: full lto compilation for mac, too

--- cmake/mac.cmake
+++ cmake/mac.cmake
@@ -3,10 +3,8 @@ set(PLATFORM mac)
 target_compile_options(${PROJECT} BEFORE PRIVATE -mmacosx-version-min=10.13 -Wshadow)
 
 if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
-    target_compile_options(${PROJECT} BEFORE PRIVATE -Ofast)
-    target_compile_options(${PROJECT} BEFORE PRIVATE -msse2)
-
-    target_link_options(${PROJECT} BEFORE PRIVATE -flto)
+    target_compile_options(${PROJECT} BEFORE PRIVATE -msse2 -Ofast -flto)
+    target_link_options(${PROJECT} BEFORE PRIVATE -msse2 -Ofast -flto)
 endif()
 
 target_link_options(${PROJECT} BEFORE PRIVATE "SHELL:-framework Appkit")
--- qmake/mac.pri
+++ qmake/mac.pri
@@ -1,6 +1,6 @@
 
 QMAKE_CXXFLAGS += -mmacosx-version-min=10.13 -Wshadow
-QMAKE_CXXFLAGS_RELEASE += -msse2 -Ofast
-QMAKE_LFLAGS_RELEASE += -flto
+QMAKE_CXXFLAGS_RELEASE += -msse2 -Ofast -flto
+QMAKE_LFLAGS_RELEASE += -msse2 -Ofast -flto
 
 QMAKE_LFLAGS += -framework Foundation -framework Appkit
