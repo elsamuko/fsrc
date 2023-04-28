4141adc3611c32efbe61dc90e0f3fcfb9ebb7664
Samuel Albrecht <elsamuko@gmail.com>
2023-04-28 18:57:13 +0200

    fix: cmake options, use mischasan as fastest string search algo

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,14 +1,24 @@
 
+# NO_THREADPOOL, run single threaded
+# OWN_THREADPOOL, use own busy pool
+# BOOST_THREADPOOL, use boost::asio
+# ASYNC_THREADPOOL, use std::async
+
+# FIND_MISCHASAN, use mischasan's sse optimized string search
+# FIND_SSE_OWN, use own sse optimized string search
+# FIND_TRAITS, use traits search from basic_string.tcc
+# FIND_STRSTR, use builtin strstr
+
 if(APPLE)
     # on macOS, use own threadpool
     add_definitions(-DTHREADPOOL=OWN_THREADPOOL)
-    add_definitions(-DFIND_ALGO=FIND_STRSTR)
+    add_definitions(-DFIND_ALGO=FIND_MISCHASAN)
 endif()
 
 if(UNIX AND NOT APPLE)
-    # on Linux, use boost::asio
+    # on Linux, use own threadpool
     add_definitions(-DTHREADPOOL=OWN_THREADPOOL)
-    add_definitions(-DFIND_ALGO=FIND_STRSTR)
+    add_definitions(-DFIND_ALGO=FIND_MISCHASAN)
 endif()
 
 if(WIN32)
